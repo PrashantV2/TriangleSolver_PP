3f19db5b58357100250995696fc6b6c389a3d713
PrashantV2 <prashantvirdi719@gmail.com>
2023-06-08 22:27:43 -0400

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+using System.Security.Policy;
+
+namespace TestClass
+{
+    public class Class1
+    {
+
+    }
+}
